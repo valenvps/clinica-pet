d452670b67378fb11b563bc746bf034d4ba8efd8
Valentina Vianna <vava.vianna.eu@hotmail.com>
2026-04-26 12:28:15 -0300

    Adicionando os CrossOrigins

--- src/main/java/br/com/valentina/clinicapet/controller/PetController.java
+++ src/main/java/br/com/valentina/clinicapet/controller/PetController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/pets")
 public class PetController {
 
@@ -16,25 +17,21 @@ public class PetController {
         this.petService = petService;
     }
 
-    // LISTAR
     @GetMapping
     public List<Pet> listar() {
         return petService.listar();
     }
 
-    // BUSCAR POR ID
     @GetMapping("/{id}")
     public Pet buscar(@PathVariable Long id) {
         return petService.buscarPorId(id);
     }
 
-    // CADASTRAR
     @PostMapping
     public Pet cadastrar(@RequestBody Pet pet) {
         return petService.salvar(pet);
     }
 
-    // DELETAR
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Long id) {
         petService.deletar(id);
